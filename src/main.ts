93baca8e43f831d0c713c6be7abd0f9e2d844566
AP <potdevin.anthony@gmail.com>
2019-11-02 23:04:07 +0100

    feat: add typescript

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ var { GraphQLObjectType } = require("graphql");
 var { GraphQLString } = require("graphql");
 
 const depthLimit = require("graphql-depth-limit");
-const logger = require("./helpers/logger");
+const { logger } = require("./helpers/logger");
 const { getIp } = require("./helpers/helpers");
 const lnService = require("ln-service");
 require("dotenv").config();
@@ -22,7 +22,7 @@ const GraphqlSchema = new GraphQLSchema({
       warning: {
         type: GraphQLString,
         resolve: () => {
-          return "You have a warning.";
+          return "You have a warning.1312323";
         }
       },
       error: {
@@ -41,14 +41,14 @@ const { lnd } = lnService.authenticatedLndGrpc({
 });
 logger.info("Connection established with gRPC");
 
-lnService.getWalletInfo({ lnd }, (error, result) => {
+lnService.getWalletInfo({ lnd }, (error: any, result: any) => {
   logger.info(`Connected to node: ${result.alias}`);
   if (error) logger.error(`Error connecting to node: ${error}`);
 });
 
 const server = new ApolloServer({
   schema: GraphqlSchema,
-  context: async ({ req }) => {
+  context: async ({ req }: any) => {
     const ip = getIp(req);
     return { ip, lnd };
   },
@@ -57,6 +57,11 @@ const server = new ApolloServer({
   ]
 });
 
-server.listen({ port: process.env.PORT || 3001 }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 3001 }).then(({ url }: any) => {
   logger.info(`Server ready at ${url}`);
 });
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => server.stop());
+}
