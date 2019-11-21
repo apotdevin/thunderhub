c9502c05d9a7d800203ba3936c18a549b204916f
AP <potdevin.anthony@gmail.com>
2019-11-21 06:49:43 +0100

    chore: small changes

--- src/schemaTypes/query/info/pendingChannels.ts
+++ src/schemaTypes/query/info/pendingChannels.ts
@@ -5,45 +5,24 @@ import {
   GraphQLList
 } from "graphql";
 import { GraphQLInt } from "graphql";
+import { PartnerNodeType } from "./channels";
 
 // TODO: INCOMPLETE TYPE
 export const PendingChannelType = new GraphQLObjectType({
   name: "pendingChannelType",
   fields: () => {
     return {
-      closeTransactionId: {
-        type: GraphQLString
-      },
-      isActive: {
-        type: GraphQLBoolean
-      },
-      isClosing: {
-        type: GraphQLBoolean
-      },
-      isOpening: {
-        type: GraphQLBoolean
-      },
-      localBalance: {
-        type: GraphQLInt
-      },
-      localReserve: {
-        type: GraphQLInt
-      },
-      partnerPublicKey: {
-        type: GraphQLString
-      },
-      received: {
-        type: GraphQLInt
-      },
-      remoteBalance: {
-        type: GraphQLInt
-      },
-      remoteReserve: {
-        type: GraphQLInt
-      },
-      sent: {
-        type: GraphQLInt
-      }
+      isActive: { type: GraphQLBoolean },
+      isClosing: { type: GraphQLBoolean },
+      isOpening: { type: GraphQLBoolean },
+      localBalance: { type: GraphQLInt },
+      localReserve: { type: GraphQLInt },
+      partnerPublicKey: { type: GraphQLString },
+      received: { type: GraphQLInt },
+      remoteBalance: { type: GraphQLInt },
+      remoteReserve: { type: GraphQLInt },
+      sent: { type: GraphQLInt },
+      partnerNodeInfo: { type: PartnerNodeType }
     };
   }
 });
