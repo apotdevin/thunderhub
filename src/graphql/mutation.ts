64d2e9b7b1ad07fa474022809cd3bdd56360cb4f
AP <potdevin.anthony@gmail.com>
2019-11-30 22:27:08 +0100

    chore: add auth to mutations

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,36 +1,38 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const CLOSE_CHANNEL = gql`
-  mutation CloseChannel(
-    $id: String!
-    $forceClose: Boolean
-    $target: Int
-    $tokens: Int
-  ) {
-    closeChannel(
-      id: $id
-      forceClose: $forceClose
-      targetConfirmations: $target
-      tokensPerVByte: $tokens
+    mutation CloseChannel(
+        $id: String!
+        $auth: String!
+        $forceClose: Boolean
+        $target: Int
+        $tokens: Int
     ) {
-      transactionId
-      transactionOutputIndex
+        closeChannel(
+            id: $id
+            forceClose: $forceClose
+            targetConfirmations: $target
+            tokensPerVByte: $tokens
+            auth: $auth
+        ) {
+            transactionId
+            transactionOutputIndex
+        }
     }
-  }
 `;
 
 export const PAY_INVOICE = gql`
-  mutation PayInvoice($request: String!) {
-    pay(request: $request) {
-      isConfirmed
+    mutation PayInvoice($request: String!, $auth: String!) {
+        pay(request: $request, auth: $auth) {
+            isConfirmed
+        }
     }
-  }
 `;
 
 export const CREATE_INVOICE = gql`
-  mutation PayInvoice($amount: Int!) {
-    createInvoice(amount: $amount) {
-      request
+    mutation PayInvoice($amount: Int!, $auth: String!) {
+        createInvoice(amount: $amount, auth: $auth) {
+            request
+        }
     }
-  }
 `;
