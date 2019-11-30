49e76f322dc0206870958bb9df2c9a79d4ae570d
AP <potdevin.anthony@gmail.com>
2019-11-30 11:50:49 +0100

    chore: prettier

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -6,7 +6,9 @@ export const getIp = (req: any) => {
         return '';
     }
     const forwarded = req.headers['x-forwarded-for'];
-    const before = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+    const before = forwarded
+        ? forwarded.split(/, /)[0]
+        : req.connection.remoteAddress;
     const ip = process.env.NODE_ENV === 'development' ? '1.2.3.4' : before;
     return ip;
 };
