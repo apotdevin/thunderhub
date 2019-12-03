ac2349c60c609337331a5d89e48a1e909c38e250
AP <potdevin.anthony@gmail.com>
2019-12-03 19:43:36 +0100

    chore: cleanup interfaces

--- src/schemas/query/flow/getInOut.interface.ts
+++ src/schemas/query/flow/getInOut.interface.ts
@@ -5,3 +5,24 @@ export interface InOutProps {
 export interface InOutListProps {
     [key: string]: InOutProps[];
 }
+
+export interface PaymentProps {
+    created_at: string;
+    is_confirmed: boolean;
+    tokens: number;
+}
+
+export interface PaymentsProps {
+    payments: PaymentProps[];
+}
+
+export interface InvoiceProps {
+    created_at: string;
+    is_confirmed: boolean;
+    received: number;
+}
+
+export interface InvoicesProps {
+    invoices: InvoiceProps[];
+    next: string;
+}
