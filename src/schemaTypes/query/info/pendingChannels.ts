406d8300631fb8d829a023fed697d83f9377ed5e
AP <potdevin.anthony@gmail.com>
2019-12-07 18:13:29 +0100

    chore: more info for pending channels

--- src/schemaTypes/query/info/pendingChannels.ts
+++ src/schemaTypes/query/info/pendingChannels.ts
@@ -1,28 +1,31 @@
 import {
-  GraphQLObjectType,
-  GraphQLBoolean,
-  GraphQLString,
-  GraphQLList
-} from "graphql";
-import { GraphQLInt } from "graphql";
-import { PartnerNodeType } from "./channels";
+    GraphQLObjectType,
+    GraphQLBoolean,
+    GraphQLString,
+} from 'graphql';
+import { GraphQLInt } from 'graphql';
+import { PartnerNodeType } from './channels';
 
 // TODO: INCOMPLETE TYPE
 export const PendingChannelType = new GraphQLObjectType({
-  name: "pendingChannelType",
-  fields: () => {
-    return {
-      isActive: { type: GraphQLBoolean },
-      isClosing: { type: GraphQLBoolean },
-      isOpening: { type: GraphQLBoolean },
-      localBalance: { type: GraphQLInt },
-      localReserve: { type: GraphQLInt },
-      partnerPublicKey: { type: GraphQLString },
-      received: { type: GraphQLInt },
-      remoteBalance: { type: GraphQLInt },
-      remoteReserve: { type: GraphQLInt },
-      sent: { type: GraphQLInt },
-      partnerNodeInfo: { type: PartnerNodeType }
-    };
-  }
+    name: 'pendingChannelType',
+    fields: () => {
+        return {
+            closeTransactionId: { type: GraphQLString },
+            isActive: { type: GraphQLBoolean },
+            isClosing: { type: GraphQLBoolean },
+            isOpening: { type: GraphQLBoolean },
+            localBalance: { type: GraphQLInt },
+            localReserve: { type: GraphQLInt },
+            partnerPublicKey: { type: GraphQLString },
+            received: { type: GraphQLInt },
+            remoteBalance: { type: GraphQLInt },
+            remoteReserve: { type: GraphQLInt },
+            sent: { type: GraphQLInt },
+            partnerNodeInfo: { type: PartnerNodeType },
+            transactionFee: { type: GraphQLInt },
+            transactionId: { type: GraphQLString },
+            transactionVout: { type: GraphQLInt },
+        };
+    },
 });
