058bb84ab876d0b2c084deca743c6d6787c3f9bd
AP <potdevin.anthony@gmail.com>
2019-12-08 14:26:33 +0100

    chore: refactor error msg

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -13,36 +13,15 @@ export const getIp = (req: any) => {
     return ip;
 };
 
-export const getBase64CertfromDerFormat = (url: string) => {
-    if (!url) return null;
-
-    const base64 = base64url.toBase64(url);
-
-    const prefix = '-----BEGIN CERTIFICATE-----\n';
-    const postfix = '-----END CERTIFICATE-----';
-    const pem = base64.match(/.{0,64}/g) || [];
-    const pemString = pem.join('\n');
-    const pemComplete = prefix + pemString + postfix;
-    const pemText = base64url.encode(pemComplete);
-
-    return pemText;
-};
-
 export const getAuthLnd = (auth: string) => {
     const url = new URL(auth);
 
-    // console.log(url);
     const encodedCert = url.searchParams.get('cert') || '';
     const encodedMacaroon = url.searchParams.get('macaroon') || '';
     const socket = url.host;
 
-    const cert = getBase64CertfromDerFormat(encodedCert);
-    // const macaroon = base64url.toBase64(encodedMacaroon);
-
-    // const cert = base64url.toBase64(encodedCert);
+    const cert = base64url.toBase64(encodedCert);
     const macaroon = base64url.toBase64(encodedMacaroon).replace('==', '');
-
-    // console.log(encodedCert);
 
     const params =
         encodedCert !== ''
@@ -61,7 +40,15 @@ export const getAuthLnd = (auth: string) => {
 export const getErrorMsg = (error: any[]): string => {
     const code = error[0];
     const msg = error[1];
-    const details = error.length > 2 ? error[2].err.details : '';
+
+    let details = ''
+    if (error.length > 2) {
+        if (error[2].err) {
+            details = error[2].err.details
+        } else if(error[2].details) {
+            details = error[2].details
+        }
+    }
 
     return JSON.stringify({ code, msg, details });
 };
