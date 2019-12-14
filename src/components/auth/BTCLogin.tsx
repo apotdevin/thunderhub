37ec4a78d3f7028caecf00389899470339d1cbe3
AP <potdevin.anthony@gmail.com>
2019-12-14 13:43:33 +0100

    feat: test connection before hand

--- src/components/auth/BTCLogin.tsx
+++ src/components/auth/BTCLogin.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, SingleLine, Sub4Title } from '../../components/generic/Styled';
 import { useAccount } from '../../context/AccountContext';
-import { getConfigLnd, saveUserAuth } from '../../utils/auth';
+import { getConfigLnd, saveUserAuth, getAuthString } from '../../utils/auth';
 import CryptoJS from 'crypto-js';
 import { LoginButton, PasswordInput } from './Password';
 import { toast } from 'react-toastify';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { GET_CAN_CONNECT } from '../../graphql/query';
+import { getErrorContent } from '../../utils/error';
 
 interface AuthProps {
-    available?: number;
+    available: number;
     callback?: () => void;
 }
 
@@ -20,9 +23,54 @@ export const BTCLoginForm = ({ available, callback }: AuthProps) => {
     const [hasInfo, setHasInfo] = useState(false);
     const [isPass, setPass] = useState('');
 
-    if (!available) return null;
+    const [tryToConnect, { data, loading }] = useLazyQuery(GET_CAN_CONNECT, {
+        onError: error => toast.error(getErrorContent(error)),
+    });
 
-    const canConnect = isJson !== '' && !!available;
+    useEffect(() => {
+        if (!loading && data && data.getNodeInfo && data.getNodeInfo.alias) {
+            const { cert, macaroon, readMacaroon, host } = getConfigLnd(isJson);
+
+            if (!host) {
+                toast.error('Invalid connection credentials');
+                return;
+            }
+
+            const encryptedAdmin =
+                macaroon && isPass !== ''
+                    ? CryptoJS.AES.encrypt(macaroon, isPass).toString()
+                    : undefined;
+
+            saveUserAuth({
+                available,
+                name: isName,
+                host,
+                admin: encryptedAdmin,
+                read: readMacaroon,
+                cert,
+            });
+
+            setAccount({
+                loggedIn: true,
+                host,
+                admin: macaroon,
+                read: readMacaroon,
+                cert,
+            });
+
+            toast.success('Connected!');
+            callback && callback();
+        }
+    }, [
+        data,
+        loading,
+        available,
+        callback,
+        isJson,
+        isName,
+        isPass,
+        setAccount,
+    ]);
 
     const handleClick = () => {
         try {
@@ -34,66 +82,50 @@ export const BTCLoginForm = ({ available, callback }: AuthProps) => {
     };
 
     const handleConnect = () => {
-        const { cert, macaroon, readMacaroon, host } = getConfigLnd(isJson);
-
-        const encryptedAdmin =
-            macaroon && isPass !== ''
-                ? CryptoJS.AES.encrypt(macaroon, isPass).toString()
-                : undefined;
+        const { cert, readMacaroon, host } = getConfigLnd(isJson);
 
         if (!host) {
             toast.error('Invalid connection credentials');
             return;
         }
 
-        saveUserAuth({
-            available,
-            name: isName,
-            host,
-            admin: encryptedAdmin,
-            read: readMacaroon,
-            cert,
+        tryToConnect({
+            variables: { auth: getAuthString(host, readMacaroon, cert) },
         });
-
-        setAccount({
-            loggedIn: true,
-            host,
-            admin: macaroon,
-            read: readMacaroon,
-            cert,
-        });
-
-        callback && callback();
     };
 
-    const renderContent = () => (
-        <>
-            <SingleLine>
-                <Sub4Title>Name:</Sub4Title>
-                <Input onChange={e => setName(e.target.value)} />
-            </SingleLine>
-            <SingleLine>
-                <Sub4Title>BTCPay Connect Url:</Sub4Title>
-                <Input onChange={e => setJson(e.target.value)} />
-            </SingleLine>
-            {canConnect && (
-                <LoginButton
-                    disabled={!canConnect}
-                    enabled={canConnect}
-                    onClick={handleClick}
-                    color={'yellow'}
-                >
-                    Connect
-                </LoginButton>
-            )}
-        </>
-    );
+    const renderContent = () => {
+        const canConnect = isJson !== '' && !!available;
+        return (
+            <>
+                <SingleLine>
+                    <Sub4Title>Name:</Sub4Title>
+                    <Input onChange={e => setName(e.target.value)} />
+                </SingleLine>
+                <SingleLine>
+                    <Sub4Title>BTCPay Connect Url:</Sub4Title>
+                    <Input onChange={e => setJson(e.target.value)} />
+                </SingleLine>
+                {canConnect && (
+                    <LoginButton
+                        disabled={!canConnect}
+                        enabled={canConnect}
+                        onClick={handleClick}
+                        color={'yellow'}
+                    >
+                        Connect
+                    </LoginButton>
+                )}
+            </>
+        );
+    };
 
     return hasInfo ? (
         <PasswordInput
             isPass={isPass}
             setPass={setPass}
             callback={handleConnect}
+            loading={loading}
         />
     ) : (
         renderContent()
--- src/components/auth/NormalLogin.tsx
+++ src/components/auth/NormalLogin.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, SingleLine, Sub4Title } from '../generic/Styled';
 import { useAccount } from '../../context/AccountContext';
-import { saveUserAuth } from '../../utils/auth';
+import { saveUserAuth, getAuthString } from '../../utils/auth';
 import CryptoJS from 'crypto-js';
 import base64url from 'base64url';
 import { PasswordInput, LoginButton } from './Password';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { GET_CAN_CONNECT } from '../../graphql/query';
+import { getErrorContent } from '../../utils/error';
+import { toast } from 'react-toastify';
 
 interface AuthProps {
-    available?: number;
+    available: number;
     callback?: () => void;
 }
 
@@ -23,10 +27,9 @@ export const LoginForm = ({ available, callback }: AuthProps) => {
     const [hasInfo, setHasInfo] = useState(false);
     const [isPass, setPass] = useState('');
 
-    if (!available) return null;
-
-    const canConnect =
-        isName !== '' && isHost !== '' && isRead !== '' && !!available;
+    const [tryToConnect, { data, loading }] = useLazyQuery(GET_CAN_CONNECT, {
+        onError: error => toast.error(getErrorContent(error)),
+    });
 
     const handleClick = () => {
         if (isAdmin !== '') {
@@ -36,76 +39,110 @@ export const LoginForm = ({ available, callback }: AuthProps) => {
         }
     };
 
+    useEffect(() => {
+        if (!loading && data && data.getNodeInfo && data.getNodeInfo.alias) {
+            const admin = base64url.fromBase64(isAdmin);
+            const read = base64url.fromBase64(isRead);
+            const cert = base64url.fromBase64(isCert);
+
+            const encryptedAdmin =
+                admin && isPass !== ''
+                    ? CryptoJS.AES.encrypt(admin, isPass).toString()
+                    : undefined;
+
+            saveUserAuth({
+                available,
+                name: isName,
+                host: isHost,
+                admin: encryptedAdmin,
+                read,
+                cert,
+            });
+
+            setAccount({
+                loggedIn: true,
+                host: isHost,
+                admin: encryptedAdmin,
+                read,
+                cert,
+            });
+
+            toast.success('Connected!');
+            callback && callback();
+        }
+    }, [
+        data,
+        loading,
+        available,
+        callback,
+        isAdmin,
+        isCert,
+        isHost,
+        isName,
+        isPass,
+        isRead,
+        setAccount,
+    ]);
+
     const handleConnect = () => {
         const admin = base64url.fromBase64(isAdmin);
         const read = base64url.fromBase64(isRead);
         const cert = base64url.fromBase64(isCert);
 
-        const encryptedAdmin =
-            admin && isPass !== ''
-                ? CryptoJS.AES.encrypt(admin, isPass).toString()
-                : undefined;
+        const correctMacaroon = read ? read : admin;
 
-        saveUserAuth({
-            available,
-            name: isName,
-            host: isHost,
-            admin: encryptedAdmin,
-            read,
-            cert,
+        tryToConnect({
+            variables: {
+                auth: getAuthString(isHost, correctMacaroon, cert),
+            },
         });
-
-        setAccount({
-            loggedIn: true,
-            host: isHost,
-            admin: encryptedAdmin,
-            read,
-            cert,
-        });
-
-        callback && callback();
     };
 
-    const renderContent = () => (
-        <>
-            <SingleLine>
-                <Sub4Title>Name:</Sub4Title>
-                <Input onChange={e => setName(e.target.value)} />
-            </SingleLine>
-            <SingleLine>
-                <Sub4Title>Host:</Sub4Title>
-                <Input onChange={e => setHost(e.target.value)} />
-            </SingleLine>
-            <SingleLine>
-                <Sub4Title>Admin:</Sub4Title>
-                <Input onChange={e => setAdmin(e.target.value)} />
-            </SingleLine>
-            <SingleLine>
-                <Sub4Title>Readonly:</Sub4Title>
-                <Input onChange={e => setRead(e.target.value)} />
-            </SingleLine>
-            <SingleLine>
-                <Sub4Title>Certificate:</Sub4Title>
-                <Input onChange={e => setCert(e.target.value)} />
-            </SingleLine>
-            {canConnect && (
-                <LoginButton
-                    disabled={!canConnect}
-                    enabled={canConnect}
-                    onClick={handleClick}
-                    color={'yellow'}
-                >
-                    Connect
-                </LoginButton>
-            )}
-        </>
-    );
+    const renderContent = () => {
+        const canConnect =
+            isName !== '' && isHost !== '' && isRead !== '' && !!available;
+        return (
+            <>
+                <SingleLine>
+                    <Sub4Title>Name:</Sub4Title>
+                    <Input onChange={e => setName(e.target.value)} />
+                </SingleLine>
+                <SingleLine>
+                    <Sub4Title>Host:</Sub4Title>
+                    <Input onChange={e => setHost(e.target.value)} />
+                </SingleLine>
+                <SingleLine>
+                    <Sub4Title>Admin:</Sub4Title>
+                    <Input onChange={e => setAdmin(e.target.value)} />
+                </SingleLine>
+                <SingleLine>
+                    <Sub4Title>Readonly:</Sub4Title>
+                    <Input onChange={e => setRead(e.target.value)} />
+                </SingleLine>
+                <SingleLine>
+                    <Sub4Title>Certificate:</Sub4Title>
+                    <Input onChange={e => setCert(e.target.value)} />
+                </SingleLine>
+                {canConnect && (
+                    <LoginButton
+                        disabled={!canConnect}
+                        enabled={canConnect}
+                        onClick={handleClick}
+                        color={'yellow'}
+                    >
+                        Connect
+                    </LoginButton>
+                )}
+            </>
+        );
+    };
 
     return hasInfo ? (
         <PasswordInput
             isPass={isPass}
             setPass={setPass}
             callback={handleConnect}
+            loading={loading}
         />
     ) : (
         renderContent()
