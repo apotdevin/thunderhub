123dd904bc60913c11b66e8b9f325bbf6a472b59
Anthony Potdevin <potdevin@DEV135MAC0106.local>
2019-12-26 12:46:39 +0530

    feat: get and verify backup

---
+++ src/schemas/query/backup/verifyBackups.ts
@@ -0,0 +1,45 @@
+import { verifyBackups as verifyLnBackups } from 'ln-service';
+import { logger } from '../../../helpers/logger';
+import { requestLimiter } from '../../../helpers/rateLimiter';
+import { GraphQLNonNull, GraphQLString, GraphQLBoolean } from 'graphql';
+import { getAuthLnd, getErrorMsg } from '../../../helpers/helpers';
+
+interface BackupProps {
+    backup: string;
+    channels: {}[];
+}
+
+export const verifyBackups = {
+    type: GraphQLBoolean,
+    args: {
+        auth: { type: new GraphQLNonNull(GraphQLString) },
+        backup: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, params: any, context: any) => {
+        await requestLimiter(context.ip, 'verifyBackups');
+
+        const lnd = getAuthLnd(params.auth);
+
+        let backupObj: BackupProps = { backup: '', channels: [] };
+        try {
+            backupObj = JSON.parse(params.backup);
+        } catch (error) {
+            logger.error('Corrupt backup file: %o', error);
+            throw new Error('Corrupt backup file');
+        }
+
+        const { backup, channels } = backupObj;
+
+        try {
+            const { is_valid } = await verifyLnBackups({
+                lnd: lnd,
+                backup,
+                channels,
+            });
+            return is_valid;
+        } catch (error) {
+            logger.error('Error verifying backups: %o', error);
+            throw new Error(getErrorMsg(error));
+        }
+    },
+};
