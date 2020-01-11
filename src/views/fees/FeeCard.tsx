7a471abe67d7f123cc8c2071fbccbcd98bcd2172
AP <potdevin.anthony@gmail.com>
2020-01-11 18:25:26 +0100

    feat: add fee mutation

--- src/views/fees/FeeCard.tsx
+++ src/views/fees/FeeCard.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     SubCard,
     Separation,
     SingleLine,
     DarkSubTitle,
+    Input,
 } from '../../components/generic/Styled';
 import { renderLine } from '../../components/generic/Helpers';
 import { MainInfo, NodeTitle, ColLine } from './Fees.style';
+import { useMutation } from '@apollo/react-hooks';
+import { UPDATE_FEES } from '../../graphql/mutation';
+import { toast } from 'react-toastify';
+import { getErrorContent } from '../../utils/error';
+import styled from 'styled-components';
+import { ChevronRight } from '../../components/generic/Icons';
+import { SecureButton } from '../../components/secureButton/SecureButton';
+
+const SmallInput = styled(Input)`
+    max-width: 150px;
+`;
 
 interface FeeCardProps {
     channelInfo: any;
@@ -21,6 +33,9 @@ export const FeeCard = ({
     setIndexOpen,
     indexOpen,
 }: FeeCardProps) => {
+    const [newBaseFee, setBaseFee] = useState(0);
+    const [newFeeRate, setFeeRate] = useState(0);
+
     const {
         alias,
         color,
@@ -29,6 +44,17 @@ export const FeeCard = ({
         transactionId,
         transactionVout,
     } = channelInfo;
+
+    const [updateFees] = useMutation(UPDATE_FEES, {
+        onError: error => toast.error(getErrorContent(error)),
+        onCompleted: data => {
+            setIndexOpen(0);
+            data.updateFees
+                ? toast.success('Channel fees updated')
+                : toast.error('Error updating channel fees');
+        },
+        refetchQueries: ['GetChannelFees'],
+    });
 
     const handleClick = () => {
         if (indexOpen === index) {
@@ -44,6 +70,40 @@ export const FeeCard = ({
                 <Separation />
                 {renderLine('Transaction Id:', transactionId)}
                 {renderLine('Transaction Vout:', transactionVout)}
+                <Separation />
+                <SingleLine>
+                    <DarkSubTitle>{`Base Fee (Sats):`}</DarkSubTitle>
+                    <SmallInput
+                        color={'white'}
+                        type={'number'}
+                        onChange={e => setBaseFee(parseInt(e.target.value))}
+                    />
+                </SingleLine>
+                <SingleLine>
+                    <DarkSubTitle>{`Fee Rate (Sats/Million):`}</DarkSubTitle>
+                    <SmallInput
+                        color={'white'}
+                        type={'number'}
+                        onChange={e => setFeeRate(parseInt(e.target.value))}
+                    />
+                </SingleLine>
+                <SingleLine>
+                    <SecureButton
+                        callback={updateFees}
+                        variables={{
+                            transactionId,
+                            transactionVout,
+                            ...(newBaseFee !== 0 && { baseFee: newBaseFee }),
+                            ...(newFeeRate !== 0 && { feeRate: newFeeRate }),
+                        }}
+                        color={'white'}
+                        enabled={newBaseFee >= 0 || newFeeRate >= 0}
+                        disabled={newBaseFee === 0 && newFeeRate === 0}
+                    >
+                        Update Fees
+                        <ChevronRight />
+                    </SecureButton>
+                </SingleLine>
             </>
         );
     };
