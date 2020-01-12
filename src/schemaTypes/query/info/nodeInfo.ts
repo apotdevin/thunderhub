e4447058826dbadff7888ca0afe71ac7bd742026
AP <potdevin.anthony@gmail.com>
2020-01-12 13:28:17 +0100

    chore: node info cleanup

--- src/schemaTypes/query/info/nodeInfo.ts
+++ src/schemaTypes/query/info/nodeInfo.ts
@@ -1,57 +1,30 @@
 import {
-  GraphQLObjectType,
-  GraphQLBoolean,
-  GraphQLString,
-  GraphQLList
-} from "graphql";
-import { GraphQLInt } from "graphql";
+    GraphQLObjectType,
+    GraphQLBoolean,
+    GraphQLString,
+    GraphQLList,
+} from 'graphql';
+import { GraphQLInt } from 'graphql';
 
 export const NodeInfoType = new GraphQLObjectType({
-  name: "nodeInfoType",
-  fields: () => {
-    return {
-      chains: {
-        type: new GraphQLList(GraphQLString)
-      },
-      color: {
-        type: GraphQLString
-      },
-      activeChannelsCount: {
-        type: GraphQLInt
-      },
-      alias: {
-        type: GraphQLString
-      },
-      currentBlockHash: {
-        type: GraphQLString
-      },
-      currentBlockHeight: {
-        type: GraphQLBoolean
-      },
-      isSyncedToChain: {
-        type: GraphQLBoolean
-      },
-      isSyncedToGraph: {
-        type: GraphQLBoolean
-      },
-      latestBlockAt: {
-        type: GraphQLString
-      },
-      peersCount: {
-        type: GraphQLInt
-      },
-      pendingChannelsCount: {
-        type: GraphQLInt
-      },
-      publicKey: {
-        type: GraphQLString
-      },
-      uris: {
-        type: new GraphQLList(GraphQLString)
-      },
-      version: {
-        type: GraphQLString
-      }
-    };
-  }
+    name: 'nodeInfoType',
+    fields: () => {
+        return {
+            chains: { type: new GraphQLList(GraphQLString) },
+            color: { type: GraphQLString },
+            active_channels_count: { type: GraphQLInt },
+            closed_channels_count: { type: GraphQLInt },
+            alias: { type: GraphQLString },
+            current_block_hash: { type: GraphQLString },
+            current_block_height: { type: GraphQLBoolean },
+            is_synced_to_chain: { type: GraphQLBoolean },
+            is_synced_to_graph: { type: GraphQLBoolean },
+            latest_block_at: { type: GraphQLString },
+            peers_count: { type: GraphQLInt },
+            pending_channels_count: { type: GraphQLInt },
+            public_key: { type: GraphQLString },
+            uris: { type: new GraphQLList(GraphQLString) },
+            version: { type: GraphQLString },
+        };
+    },
 });
