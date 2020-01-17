14b80cf78ab39e2287f63752b5fa2bddba6ab36d
AP <potdevin.anthony@gmail.com>
2020-01-17 07:44:37 +0100

    feat: refactor forwards query

--- src/schemaTypes/query/transactions/forwards.ts
+++ src/schemaTypes/query/transactions/forwards.ts
@@ -1,15 +1,27 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
 import { GraphQLInt } from 'graphql';
 
-export const GetForwardType = new GraphQLObjectType({
-    name: 'getForwardType',
+export const ForwardType = new GraphQLObjectType({
+    name: 'forwardType',
     fields: () => ({
         created_at: { type: GraphQLString },
         fee: { type: GraphQLInt },
         fee_mtokens: { type: GraphQLString },
         incoming_channel: { type: GraphQLString },
+        incoming_alias: { type: GraphQLString },
+        incoming_color: { type: GraphQLString },
         mtokens: { type: GraphQLString },
         outgoing_channel: { type: GraphQLString },
+        outgoing_alias: { type: GraphQLString },
+        outgoing_color: { type: GraphQLString },
         tokens: { type: GraphQLInt },
+    }),
+});
+
+export const GetForwardType = new GraphQLObjectType({
+    name: 'getForwardType',
+    fields: () => ({
+        token: { type: GraphQLString },
+        forwards: { type: new GraphQLList(ForwardType) },
     }),
 });
