03d4bc648de1e6f38533a6b31d7d196e04ac2524
AnthonyPotdevin <anthonypotdevin@MacBook-Pro-de-AnthonyPotdevin.local>
2020-01-19 16:22:41 +0100

    chore: price query refactor

--- src/schemas/query/data/bitcoinPrice.ts
+++ src/schemas/query/data/bitcoinPrice.ts
@@ -1,13 +1,12 @@
 import { logger } from '../../../helpers/logger';
 import { requestLimiter } from '../../../helpers/rateLimiter';
 import { GraphQLString } from 'graphql';
-import { BitcoinPriceType } from '../../../schemaTypes/query/data/bitcoinPrice';
 import fetch from 'node-fetch';
 
 const url = 'https://blockchain.info/ticker';
 
 export const getBitcoinPrice = {
-    type: BitcoinPriceType,
+    type: GraphQLString,
     args: {
         currency: {
             type: GraphQLString,
@@ -20,23 +19,7 @@ export const getBitcoinPrice = {
             const response = await fetch(url);
             const json = await response.json();
 
-            if (!params.currency && 'EUR' in json) {
-                const value = json.EUR;
-
-                return {
-                    price: value.last,
-                    symbol: value.symbol,
-                };
-            } else if (params.currency in json) {
-                const value = json[params.currency];
-
-                return {
-                    price: value.last,
-                    symbol: value.symbol,
-                };
-            } else {
-                throw new Error('Problem getting Bitcoin price.');
-            }
+            return JSON.stringify(json);
         } catch (error) {
             logger.error('Error getting bitcoin price: %o', error);
             throw new Error('Problem getting Bitcoin price.');
