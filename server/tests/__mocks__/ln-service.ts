3b511e637bbbdd1d5903c47a0ac4fe717fda963c
AP <potdevin.anthony@gmail.com>
2020-06-14 14:49:54 +0200

    test: 🚨 query tests

--- server/tests/__mocks__/ln-service.ts
+++ server/tests/__mocks__/ln-service.ts
@@ -1,91 +1,93 @@
-export const getNetworkInfo = () =>
-  Promise.resolve({
-    average_channel_size: 1000,
-    channel_count: 123,
-    max_channel_size: 456,
-    median_channel_size: 789,
-    min_channel_size: 100,
-    node_count: 10000,
-    not_recently_updated_policy_count: 10,
-    total_capacity: 10000,
-  });
-
-export const getWalletInfo = () =>
-  Promise.resolve({
-    chains: ['abc', 'def'],
-    color: 'color',
-    active_channels_count: 1,
-    alias: 'TestNode',
-    current_block_hash: 'asd',
-    current_block_height: 123456,
-    is_synced_to_chain: true,
-    is_synced_to_graph: true,
-    latest_block_at: 'time',
-    peers_count: 1,
-    pending_channels_count: 2,
-    public_key: 'key',
-    uris: ['uri', 'uri2'],
-    version: 'version',
-  });
-
-export const getClosedChannels = () =>
-  Promise.resolve({
-    channels: [
-      {
-        capacity: 10000,
-        close_confirm_height: 123,
-        close_transaction_id: 'fghijk',
-        final_local_balance: 123,
-        final_time_locked_balance: 123,
-        id: 'id',
-        is_breach_close: false,
-        is_cooperative_close: true,
-        is_funding_cancel: false,
-        is_local_force_close: false,
-        is_remote_force_close: false,
-        partner_public_key: 'abcdef',
-        transaction_id: '123',
-        transaction_vout: 1,
-      },
-      {
-        capacity: 10000,
-        close_confirm_height: 123,
-        close_transaction_id: 'fghijk',
-        final_local_balance: 123,
-        final_time_locked_balance: 123,
-        id: 'id',
-        is_breach_close: false,
-        is_cooperative_close: true,
-        is_funding_cancel: false,
-        is_local_force_close: false,
-        is_remote_force_close: false,
-        partner_public_key: 'abcdef',
-        transaction_id: '123',
-        transaction_vout: 1,
-      },
-      {
-        capacity: 10000,
-        close_confirm_height: 123,
-        close_transaction_id: 'fghijk',
-        final_local_balance: 123,
-        final_time_locked_balance: 123,
-        id: 'id',
-        is_breach_close: false,
-        is_cooperative_close: true,
-        is_funding_cancel: false,
-        is_local_force_close: false,
-        is_remote_force_close: false,
-        partner_public_key: 'abcdef',
-        transaction_id: '123',
-        transaction_vout: 1,
-      },
-    ],
-  });
-
-export const getChainBalance = () =>
-  Promise.resolve({ chain_balance: 1000000 });
-
-export const getPendingChainBalance = () =>
-  Promise.resolve({ pending_chain_balance: 1000000 });
+import * as res from '../lnServiceResponse';
 
 export const authenticatedLndGrpc = jest.fn().mockReturnValue({});
+
+export const getNetworkInfo = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getNetworkInfoResponse));
+
+export const getWalletInfo = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getWalletInfoResponse));
+
+export const getClosedChannels = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getClosedChannelsResponse));
+
+export const getChainBalance = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getChainBalanceResponse));
+
+export const getPendingChainBalance = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getPendingChainBalanceResponse));
+
+export const pay = jest.fn().mockReturnValue(Promise.resolve(res.payResponse));
+
+export const decodePaymentRequest = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.decodePaymentRequestResponse));
+
+export const getBackups = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getBackupsResponse));
+
+export const getChainTransactions = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getChainTransactionsResponse));
+
+export const getChannels = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getChannelsResponse));
+
+export const getChannel = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getChannelResponse));
+
+export const getForwards = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getForwardsResponse));
+
+export const getInvoices = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getInvoicesResponse));
+
+export const getChannelBalance = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getChannelBalance));
+
+export const getPeers = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getPeersResponse));
+
+export const getPendingChannels = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getPendingChannelsResponse));
+
+export const getPayments = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getPayments));
+
+export const getUtxos = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getUtxosResponse));
+
+export const getWalletVersion = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.getWalletVersionResponse));
+
+export const recoverFundsFromChannels = jest
+  .fn()
+  .mockReturnValue(Promise.resolve());
+
+export const signMessage = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.signMessageResponse));
+
+export const verifyBackups = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.verifyBackupsResponse));
+
+export const verifyMessage = jest
+  .fn()
+  .mockReturnValue(Promise.resolve(res.verifyMessageResponse));
