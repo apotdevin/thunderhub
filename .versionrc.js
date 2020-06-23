9a612224d34577ac98124731d35292e0f4636e8f
AP <potdevin.anthony@gmail.com>
2020-06-23 11:01:26 +0200

    chore: 🔧 improve changelog

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features', hidden: false },
+    { type: 'chore', section: 'Improvements', hidden: false },
+    { type: 'fix', section: 'Bug Fixes', hidden: false },
+    { type: 'refactor', section: 'Refactoring', hidden: false },
+    { type: 'perf', section: 'Performance', hidden: false },
+    { type: 'revert', section: 'Reverts', hidden: false },
+    { type: 'docs', section: 'Docs', hidden: false },
+    { type: 'style', section: 'Styling', hidden: false },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'build', section: 'Build', hidden: false },
+    { type: 'ci', section: 'CI', hidden: false },
+  ],
+};
