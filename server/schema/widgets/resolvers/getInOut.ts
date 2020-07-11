fe551c33c24ff32b0e1a49c1010e3270b33d0745
AP <potdevin.anthony@gmail.com>
2020-07-11 23:30:43 +0200

    refactor: ♻️ transaction report

--- server/schema/widgets/resolvers/getInOut.ts
+++ server/schema/widgets/resolvers/getInOut.ts
@@ -1,19 +1,11 @@
-import {
-  getInvoices as getLnInvoices,
-  getPayments as getLnPayments,
-} from 'ln-service';
+import { getInvoices, getPayments } from 'ln-service';
 import { differenceInHours, differenceInCalendarDays } from 'date-fns';
 import { groupBy } from 'underscore';
 import { ContextType } from 'server/types/apiTypes';
-import { logger } from 'server/helpers/logger';
 import { requestLimiter } from 'server/helpers/rateLimiter';
-import {
-  getAuthLnd,
-  getErrorMsg,
-  getCorrectAuth,
-} from 'server/helpers/helpers';
+import { getAuthLnd, getCorrectAuth } from 'server/helpers/helpers';
+import { to } from 'server/helpers/async';
 import { reduceInOutArray } from './helpers';
-import { InvoicesProps, PaymentsProps } from './interface';
 
 export const getInOut = async (
   _: undefined,
@@ -27,65 +19,69 @@ export const getInOut = async (
 
   const endDate = new Date();
   let periods = 7;
-  let differenceFn = differenceInCalendarDays;
+  let difference = (date: string) =>
+    differenceInCalendarDays(endDate, new Date(date));
 
-  if (params.time === 'month') {
-    periods = 30;
+  if (params.time === 'year') {
+    periods = 360;
+  } else if (params.time === 'half_year') {
+    periods = 180;
+  } else if (params.time === 'quarter_year') {
+    periods = 90;
+  } else if (params.time === 'month') {
+    periods = 150;
   } else if (params.time === 'day') {
     periods = 24;
-    differenceFn = differenceInHours;
+    difference = (date: string) => differenceInHours(endDate, new Date(date));
   }
 
-  let invoiceList: InvoicesProps;
-  let paymentList: PaymentsProps;
+  const invoiceList = await to(getInvoices({ lnd, limit: 50 }));
+  const paymentList = await to(getPayments({ lnd }));
 
-  try {
-    invoiceList = await getLnInvoices({
-      lnd,
-    });
-    paymentList = await getLnPayments({
-      lnd,
-    });
-  } catch (error) {
-    logger.error('Error getting invoices: %o', error);
-    throw new Error(getErrorMsg(error));
+  let invoiceArray = invoiceList.invoices;
+  let next = invoiceList.next;
+  let finishedFetching = false;
+
+  if (!next || !invoiceArray || invoiceArray.length <= 0) {
+    finishedFetching = true;
   }
 
-  const invoices = invoiceList.invoices.map(invoice => ({
-    createdAt: invoice.created_at,
-    isConfirmed: invoice.is_confirmed,
-    tokens: invoice.received,
-  }));
+  while (!finishedFetching) {
+    const lastInvoice = invoiceArray[invoiceArray.length - 1];
+    const dif = difference(lastInvoice.created_at);
 
-  const payments = paymentList.payments.map(payment => ({
-    createdAt: payment.created_at,
-    isConfirmed: payment.is_confirmed,
-    tokens: payment.tokens,
-  }));
+    if (next && dif < periods) {
+      const newInvoices = await to(getInvoices({ lnd, token: next }));
+      invoiceArray = [...invoiceArray, ...newInvoices.invoices];
+      next = newInvoices.next;
+    } else {
+      finishedFetching = true;
+    }
+  }
 
-  const confirmedInvoices = invoices.filter(invoice => {
-    const dif = differenceFn(endDate, new Date(invoice.createdAt));
-    return invoice.isConfirmed && dif < periods;
-  });
-  const confirmedPayments = payments.filter(payment => {
-    const dif = differenceFn(endDate, new Date(payment.createdAt));
-    return payment.isConfirmed && dif < periods;
-  });
+  const allInvoices = invoiceArray.filter(
+    i => difference(i.created_at) < periods
+  );
 
-  const allInvoices = invoices.filter(invoice => {
-    const dif = differenceFn(endDate, new Date(invoice.createdAt));
-    return dif < periods;
-  });
+  const invoices = allInvoices
+    .filter(i => i.is_confirmed)
+    .map(i => ({ tokens: i.received, created_at: i.created_at }));
 
-  const totalConfirmed = confirmedInvoices.length;
+  const payments = paymentList.payments
+    .filter(p => p.is_confirmed && difference(p.created_at) < periods)
+    .map(p => ({ tokens: p.tokens, created_at: p.created_at }));
+
+  const totalConfirmed = invoices.length;
   const totalUnConfirmed = allInvoices.length - totalConfirmed;
 
-  const orderedInvoices = groupBy(confirmedInvoices, invoice => {
-    return periods - differenceFn(endDate, new Date(invoice.createdAt));
-  });
-  const orderedPayments = groupBy(confirmedPayments, payment => {
-    return periods - differenceFn(endDate, new Date(payment.createdAt));
-  });
+  const orderedInvoices = groupBy(
+    invoices,
+    invoice => periods - difference(invoice.created_at)
+  );
+  const orderedPayments = groupBy(
+    payments,
+    payment => periods - difference(payment.created_at)
+  );
 
   const reducedInvoices = reduceInOutArray(orderedInvoices);
   const reducedPayments = reduceInOutArray(orderedPayments);
