ab567e60acf943c4ab1849f7a94a35b0f72c355e
AP <potdevin.anthony@gmail.com>
2020-07-14 14:33:08 +0200

    chore: 🔧 qr code chain address

--- src/views/home/account/receiveOnChain/ReceiveOnChain.tsx
+++ src/views/home/account/receiveOnChain/ReceiveOnChain.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useCreateAddressMutation } from 'src/graphql/mutations/__generated__/createAddress.generated';
+import QRCode from 'qrcode.react';
+import { Copy } from 'react-feather';
 import {
   NoWrapTitle,
-  DarkSubTitle,
-  OverflowText,
   ResponsiveLine,
 } from '../../../../components/generic/Styled';
 import { getErrorContent } from '../../../../utils/error';
@@ -16,6 +16,40 @@ import {
   MultiButton,
   SingleButton,
 } from '../../../../components/buttons/multiButton/MultiButton';
+import { mediaWidths } from '../../../../styles/Themes';
+
+const Responsive = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (${mediaWidths.mobile}) {
+    flex-direction: column;
+  }
+`;
+
+const WrapRequest = styled.div`
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  -ms-word-break: break-all;
+  word-break: break-word;
+  margin: 24px;
+  font-size: 14px;
+`;
+
+const QRWrapper = styled.div`
+  width: 200px;
+  margin: 16px;
+`;
+
+const Column = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ButtonRow = styled.div`
   width: auto;
@@ -41,24 +75,23 @@ export const ReceiveOnChainCard = () => {
   return (
     <>
       {data && data.createAddress ? (
-        <>
-          <ResponsiveLine>
-            <DarkSubTitle>New Address:</DarkSubTitle>
-            <OverflowText>{data.createAddress}</OverflowText>
+        <Responsive>
+          <QRWrapper>
+            <QRCode value={data.createAddress} renderAs={'svg'} size={200} />
+          </QRWrapper>
+          <Column>
+            <WrapRequest>{data.createAddress}</WrapRequest>
             <CopyToClipboard
               text={data.createAddress}
               onCopy={() => toast.success('Address Copied')}
             >
-              <ColorButton
-                arrow={true}
-                withMargin={'0 0 0 16px'}
-                mobileMargin={'16px 0 0'}
-              >
+              <ColorButton>
+                <Copy size={18} />
                 Copy
               </ColorButton>
             </CopyToClipboard>
-          </ResponsiveLine>
-        </>
+          </Column>
+        </Responsive>
       ) : (
         <ResponsiveLine>
           <ButtonRow>
