2f2da04bb737f1b07196b707864129a7a083f5f5
Anthony Potdevin <31413433+apotdevin@users.noreply.github.com>
2020-09-03 07:00:22 +0200

    fix: mtokens basefee (#138)
    * fix: 🐛 updating fee policies does not support sub-satoshi values (issue #136) (#137)
    
    Co-authored-by: CSW.lol <lol@csw.lol>
    
    * chore: 🔧 cleanup
    
    Co-authored-by: initfortherekt <66783125+initfortherekt@users.noreply.github.com>
    Co-authored-by: CSW.lol <lol@csw.lol>

--- server/schema/channel/resolvers/mutation/updateFees.ts
+++ server/schema/channel/resolvers/mutation/updateFees.ts
@@ -3,9 +3,19 @@ import { ContextType } from 'server/types/apiTypes';
 import { requestLimiter } from 'server/helpers/rateLimiter';
 import { to } from 'server/helpers/async';
 
+type ParamsType = {
+  transaction_id: string;
+  transaction_vout: number;
+  base_fee_tokens: number;
+  fee_rate: number;
+  cltv_delta: number;
+  max_htlc_mtokens: string;
+  min_htlc_mtokens: string;
+};
+
 export const updateFees = async (
   _: undefined,
-  params: any,
+  params: ParamsType,
   context: ContextType
 ) => {
   await requestLimiter(context.ip, 'updateFees');
@@ -32,11 +42,13 @@ export const updateFees = async (
     throw new Error('NoDetailsToUpdateChannel');
   }
 
+  const base_fee_mtokens = Math.trunc((base_fee_tokens ?? 0) * 1000);
+
   const props = {
     lnd,
     transaction_id,
     transaction_vout,
-    ...(base_fee_tokens && { base_fee_tokens }),
+    ...(base_fee_tokens && { base_fee_mtokens }),
     ...(fee_rate && { fee_rate }),
     ...(cltv_delta && { cltv_delta }),
     ...(max_htlc_mtokens && { max_htlc_mtokens }),
