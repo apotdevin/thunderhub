c226bcee55664026efcfe5fc8755f6d5728ce788
Anthony Potdevin <potdevin.anthony@gmail.com>
2020-10-10 19:30:59 +0200

    refactor: ♻️ lnauth

--- server/schema/lnurl/resolvers.ts
+++ server/schema/lnurl/resolvers.ts
@@ -3,27 +3,13 @@ import { to } from 'server/helpers/async';
 import { logger } from 'server/helpers/logger';
 import { requestLimiter } from 'server/helpers/rateLimiter';
 import { ContextType } from 'server/types/apiTypes';
-import {
-  createInvoice,
-  decodePaymentRequest,
-  pay,
-  getWalletInfo,
-  diffieHellmanComputeSecret,
-} from 'ln-service';
+import { createInvoice, decodePaymentRequest, pay } from 'ln-service';
 import {
   CreateInvoiceType,
   DecodedType,
-  DiffieHellmanComputeSecretType,
-  GetWalletInfoType,
   PayInvoiceType,
 } from 'server/types/ln-service.types';
-
-import hmacSHA256 from 'crypto-js/hmac-sha256';
-import { enc } from 'crypto-js';
-import * as bip39 from 'bip39';
-import * as bip32 from 'bip32';
-import * as secp256k1 from 'secp256k1';
-import { BIP32Interface } from 'bip32';
+import { lnAuthUrlGenerator } from 'server/helpers/lnAuth';
 
 type LnUrlPayResponseType = {
   pr?: string;
@@ -70,14 +56,6 @@ type WithdrawRequestType = {
 type RequestType = PayRequestType | WithdrawRequestType;
 type RequestWithType = { isTypeOf: string } & RequestType;
 
-const fromHexString = (hexString: string) =>
-  new Uint8Array(
-    hexString.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) || []
-  );
-
-const toHexString = (bytes: Uint8Array) =>
-  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
-
 export const lnUrlResolvers = {
   Mutation: {
     lnUrlAuth: async (
@@ -88,65 +66,12 @@ export const lnUrlResolvers = {
       await requestLimiter(context.ip, 'lnUrl');
       const { lnd } = context;
 
-      const domainUrl = new URL(url);
-      const host = domainUrl.host;
-
-      const k1 = domainUrl.searchParams.get('k1');
-
-      if (!host || !k1) {
-        logger.error('Missing host or k1 in url: %o', url);
-        throw new Error('WrongUrlFormat');
+      if (!lnd) {
+        logger.error('Error getting authenticated LND instance in lnUrlAuth');
+        throw new Error('ProblemAuthenticatingWithLnUrlService');
       }
 
-      const wallet = await to<GetWalletInfoType>(getWalletInfo({ lnd }));
-
-      // Generate entropy
-      const secret = await to<DiffieHellmanComputeSecretType>(
-        diffieHellmanComputeSecret({
-          lnd,
-          key_family: 138,
-          key_index: 0,
-          partner_public_key: wallet?.public_key,
-        })
-      );
-
-      // Generate hash from host and entropy
-      const hashed = hmacSHA256(host, secret.secret).toString(enc.Hex);
-
-      const indexes =
-        hashed.match(/.{1,4}/g)?.map(index => parseInt(index, 16)) || [];
-
-      // Generate private seed from entropy
-      const secretKey = bip39.entropyToMnemonic(hashed);
-      const base58 = bip39.mnemonicToSeedSync(secretKey);
-
-      // Derive private seed from previous private seed and path
-      const node: BIP32Interface = bip32.fromSeed(base58);
-      const derived = node.derivePath(
-        `m/138/${indexes[0]}/${indexes[1]}/${indexes[2]}/${indexes[3]}`
-      );
-
-      // Get private and public key from derived private seed
-      const privateKey = derived.privateKey?.toString('hex');
-      const linkingKey = derived.publicKey.toString('hex');
-
-      if (!privateKey || !linkingKey) {
-        logger.error('Error deriving private or public key: %o', url);
-        throw new Error('ErrorDerivingPrivateKey');
-      }
-
-      // Sign k1 with derived private seed
-      const sigObj = secp256k1.ecdsaSign(
-        fromHexString(k1),
-        fromHexString(privateKey)
-      );
-
-      // Get signature
-      const signature = secp256k1.signatureExport(sigObj.signature);
-      const encodedSignature = toHexString(signature);
-
-      // Build final url with signature and public key
-      const finalUrl = `${url}&sig=${encodedSignature}&key=${linkingKey}`;
+      const finalUrl = await lnAuthUrlGenerator(url, lnd);
 
       try {
         const response = await fetch(finalUrl);
