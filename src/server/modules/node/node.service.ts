66e1c685b9320e18906ee48c17bdd794f009c83e
Anthony Potdevin <31413433+apotdevin@users.noreply.github.com>
2022-03-21 10:23:04 +0100

    chore: bump dependencies (#426)

--- src/server/modules/node/node.service.ts
+++ src/server/modules/node/node.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import EventEmitter from 'events';
+import {
+  CloseChannelArgs,
+  CreateChainAddressArgs,
+  CreateInvoiceArgs,
+  DiffieHellmanComputeSecretArgs,
+  GetChannelsArgs,
+  GetForwardsArgs,
+  GetInvoicesArgs,
+  GetPaymentsArgs,
+  GrantAccessArgs,
+  OpenChannelArgs,
+  PayArgs,
+  PayViaPaymentDetailsArgs,
+  SendToChainAddressArgs,
+  UpdateRoutingFeesArgs,
+  VerifyBackupsArgs,
+} from 'lightning';
 import { AccountsService } from '../accounts/accounts.service';
 import { LndService } from './lnd/lnd.service';
-import {
-  BackupChannel,
-  CloseChannelParams,
-  CreateInvoiceParams,
-  DiffieHellmanComputeSecretParams,
-  GetChannelsParams,
-  GetForwardsParams,
-  GetPaymentsParams,
-  OpenChannelParams,
-  PayInvoiceParams,
-  PayViaPaymentDetailsParams,
-  Permissions,
-  SendToChainParams,
-  UpdateRoutingFeesParams,
-} from './lnd/lnd.types';
 
 @Injectable()
 export class NodeService {
@@ -55,7 +57,7 @@ export class NodeService {
     return this.lndService.getPendingChannels(account);
   }
 
-  async getChannels(id: string, options?: GetChannelsParams) {
+  async getChannels(id: string, options?: Omit<GetChannelsArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.getChannels(account, options);
@@ -91,10 +93,10 @@ export class NodeService {
     return this.lndService.verifyBackup(account, backup);
   }
 
-  async verifyBackups(id: string, backup: string, channels: BackupChannel[]) {
+  async verifyBackups(id: string, args: Omit<VerifyBackupsArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
-    return this.lndService.verifyBackups(account, backup, channels);
+    return this.lndService.verifyBackups(account, args);
   }
 
   async recoverFundsFromChannels(id: string, backup: string) {
@@ -121,7 +123,7 @@ export class NodeService {
     return this.lndService.signMessage(account, message);
   }
 
-  async grantAccess(id: string, permissions: Permissions) {
+  async grantAccess(id: string, permissions: Omit<GrantAccessArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.grantAccess(account, permissions);
@@ -168,13 +170,20 @@ export class NodeService {
     return this.lndService.getUtxos(account);
   }
 
-  async createChainAddress(id: string, is_unused = true, format = 'p2wpkh') {
+  async createChainAddress(
+    id: string,
+    is_unused = true,
+    format: CreateChainAddressArgs['format'] = 'p2wpkh'
+  ) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.createChainAddress(account, is_unused, format);
   }
 
-  async sendToChainAddress(id: string, options: SendToChainParams) {
+  async sendToChainAddress(
+    id: string,
+    options: Omit<SendToChainAddressArgs, 'lnd'>
+  ) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.sendToChainAddress(account, options);
@@ -182,7 +191,7 @@ export class NodeService {
 
   async diffieHellmanComputeSecret(
     id: string,
-    options: DiffieHellmanComputeSecretParams
+    options: Omit<DiffieHellmanComputeSecretArgs, 'lnd'>
   ) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
@@ -195,13 +204,13 @@ export class NodeService {
     return this.lndService.decodePaymentRequest(account, request);
   }
 
-  async pay(id: string, options: PayInvoiceParams) {
+  async pay(id: string, options: Omit<PayArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.pay(account, options);
   }
 
-  async createInvoice(id: string, options: CreateInvoiceParams) {
+  async createInvoice(id: string, options: Omit<CreateInvoiceArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.createInvoice(account, options);
@@ -213,43 +222,49 @@ export class NodeService {
     return this.lndService.getChannel(account, channel_id);
   }
 
-  async closeChannel(id: string, options: CloseChannelParams) {
+  async closeChannel(id: string, options: Omit<CloseChannelArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.closeChannel(account, options);
   }
 
-  async openChannel(id: string, options: OpenChannelParams) {
+  async openChannel(id: string, options: Omit<OpenChannelArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.openChannel(account, options);
   }
 
-  async updateRoutingFees(id: string, options: UpdateRoutingFeesParams) {
+  async updateRoutingFees(
+    id: string,
+    options: Omit<UpdateRoutingFeesArgs, 'lnd'>
+  ) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.updateRoutingFees(account, options);
   }
 
-  async getForwards(id: string, options: GetForwardsParams) {
+  async getForwards(id: string, options: Omit<GetForwardsArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.getForwards(account, options);
   }
 
-  async getPayments(id: string, options: GetPaymentsParams = {}) {
+  async getPayments(id: string, options: Omit<GetPaymentsArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.getPayments(account, options);
   }
 
-  async getInvoices(id: string, options: GetPaymentsParams = {}) {
+  async getInvoices(id: string, options: Omit<GetInvoicesArgs, 'lnd'>) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.getInvoices(account, options);
   }
 
-  async payViaPaymentDetails(id: string, options: PayViaPaymentDetailsParams) {
+  async payViaPaymentDetails(
+    id: string,
+    options: Omit<PayViaPaymentDetailsArgs, 'lnd' | 'routes'>
+  ) {
     const account = this.accountsService.getAccount(id);
     if (!account) throw new Error('Node account not found');
     return this.lndService.payViaPaymentDetails(account, options);
--- src/server/utils/async.ts
+++ src/server/utils/async.ts
@@ -6,15 +6,8 @@ export const to = async <T>(promise: Promise<T>) => {
     });
 };
 
-/*
- * This is hard/impossible to type correctly. What we are describing
- * here is a set of two states: either we have a result and no error,
- * _or_ we have no result and an error. Unfortunately TypeScript is
- * not able to infer this correctly...
- * https://github.com/microsoft/TypeScript/issues/12184
- */
 export const toWithError = async <T>(promise: Promise<T>) => {
   return promise
-    .then(data => [data, undefined] as const)
-    .catch(err => [undefined, err] as const);
+    .then(data => [data, undefined] as [T, undefined])
+    .catch(err => [undefined, err] as [undefined, Error]);
 };
