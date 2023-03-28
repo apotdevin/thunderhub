4ba08070cb68a76a0ce911d98ff10466a9111d01
secondl1ght <secondl1ght@protonmail.com>
2023-03-27 19:16:50 -0600

    feat: add amboss link to node for more info

--- src/client/src/views/amboss/Billboard.tsx
+++ src/client/src/views/amboss/Billboard.tsx
@@ -6,6 +6,7 @@ import {
   Separation,
 } from '../../components/generic/Styled';
 import { Text } from '../../components/typography/Styled';
+import { Link } from '../../components/link/Link';
 
 export const Billboard = () => {
   return (
@@ -13,8 +14,15 @@ export const Billboard = () => {
       <SubTitle>Keysend Billboard</SubTitle>
       <Card>
         <Text>
-          Keysend Amboss a message and it will appear on the home page! Messages
-          are sorted by amount of sats sent and how recent it was.
+          Keysend{' '}
+          <Link
+            href="https://amboss.space/node/03006fcf3312dae8d068ea297f58e2bd00ec1ffe214b793eda46966b6294a53ce6"
+            newTab={true}
+          >
+            Amboss
+          </Link>{' '}
+          a message and it will appear on their home page! Messages are sorted
+          by amount of sats sent and how recent it was.
         </Text>
         <Separation />
         <ChatInput
