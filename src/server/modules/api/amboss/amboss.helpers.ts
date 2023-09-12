2d58cc7e2f0e53a3f26d4cf5a61d46b7b6aae190
Anthony Potdevin <potdevin.anthony@gmail.com>
2023-09-12 16:40:04 +0200

    fix: balance push

--- src/server/modules/api/amboss/amboss.helpers.ts
+++ src/server/modules/api/amboss/amboss.helpers.ts
@@ -1,3 +1,4 @@
+import { GetChannelsResult } from 'lightning';
 import { EdgeInfo, NodeAlias } from './amboss.types';
 
 export const mapNodeResult = (
@@ -14,4 +15,27 @@ export const mapEdgeResult = (
   return ids.map(
     pk => edges.find(edge => edge.short_channel_id === pk) || null
   );
+};
+
+export const getMappedChannelInfo = (
+  info: GetChannelsResult
+): {
+  chan_id: string;
+  balance: string;
+  capacity: string;
+}[] => {
+  if (!info?.channels?.length) return [];
+  return info.channels.map(c => {
+    const heldAmount = c.pending_payments.reduce((p, pp) => {
+      if (!pp) return p;
+      if (!pp.is_outgoing) return p;
+      return p + pp.tokens;
+    }, 0);
+
+    return {
+      chan_id: c.id,
+      balance: (c.local_balance + heldAmount).toString(),
+      capacity: c.capacity + '',
+    };
+  });
 };
