afb80ee206449080b396bbb3395c43d7d2bbd76b
kody.low <kodylow7@gmail.com>
2023-11-17 19:52:34 -0800

    feat: add nix flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    fedimint = {
+      url =
+        "github:fedimint/fedimint?rev=f58f1913e62b6529a2ff36ec5c89a3852aba7ca7";
+    };
   };
   outputs = { self, nixpkgs, flake-utils, fedimint }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        fmLib = fedimint.lib.${system};
       in {
         devShells = fmLib.devShells // {
           default = fmLib.devShells.default.overrideAttrs (prev: {
-            nativeBuildInputs = [ pkgs.nodejs ] ++ prev.nativeBuildInputs;
+            nativeBuildInputs = [
+              pkgs.mprocs
+              pkgs.nodejs
+              fedimint.packages.${system}.devimint
+              fedimint.packages.${system}.gateway-pkgs
+              fedimint.packages.${system}.fedimint-pkgs
+            ] ++ prev.nativeBuildInputs;
             shellHook = ''
               npm install
             '';
