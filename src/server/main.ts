650b83d2770833478cd9c03462058ee3ee5f41bc
wthrajat <rajatkhanduri290102@gmail.com>
2025-08-23 00:54:41 +0530

    chore: major dependencies bump

--- src/server/main.ts
+++ src/server/main.ts
@@ -11,4 +11,5 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000, process.env.HOST);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+void bootstrap();
