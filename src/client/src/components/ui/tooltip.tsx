0c8daefded5936da1e97d85ec931c22b0e8b63b3
Anthony Potdevin <potdevin.anthony@gmail.com>
2026-04-23 20:34:13 -0600

    chore: revert ui comp changes

--- src/client/src/components/ui/tooltip.tsx
+++ src/client/src/components/ui/tooltip.tsx
@@ -46,7 +46,7 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-xs bg-foreground fill-foreground" />
+        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );
